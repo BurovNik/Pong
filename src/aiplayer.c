463e0cad0ed0a913b160a6ae8bf9d4ecdc29484a
Burov Nikita <burov.nm@yandex.ru>
2026-08-01 15:31:36 +0300

    feat: накидал ИИ для игры, пока не знаю насколько грамотно, godemode пропущен

---
+++ src/aiplayer.c
@@ -0,0 +1,83 @@
+#include "aiplayer.h"
+#include <stdlib.h>
+#include <math.h>
+
+void aiMovePlatform(Platform *aiPlatform, const Ball *ball,const Borders *borders, AIPlayerLevel aiLevel)
+{
+    float targetY = ball->yCor - aiPlatform->heightPlatforms / 2.0f;
+   
+    switch (aiLevel)
+    {
+        case E_GODMOD:
+            aiGodMode(aiPlatform, ball, borders);
+            break;
+        case E_EASY:
+            aiEasyMode(aiPlatform, targetY, borders);
+            break;
+        case E_MEDIUM:
+            aiMediumMode(aiPlatform, targetY, borders);
+            break;
+        case E_HARD:
+            aiHardMode(aiPlatform, targetY, borders);
+            break;
+        default:
+            break;
+    }
+
+}   
+
+void aiGodMode(Platform *aiPlatform, const Ball *ball, const Borders *borders)
+{
+    return;
+}
+
+void aiEasyMode(Platform *aiPlatform, int targetY, const Borders *borders)
+{
+    int random = rand() % 4;
+    int currentY = aiPlatform->Y;
+    int needDown = (currentY < targetY);
+    if (random == 0) // в 25% случаев двигается в "неправильном" направлении
+    {
+        MovePlatform(aiPlatform, borders, !needDown);
+    }
+    else if (random == 3) // в 25% случаев двигается куда надо
+    {
+        MovePlatform(aiPlatform, borders, needDown);
+    }
+    // 1,2 ничего не делаем
+    return;
+}
+
+void aiMediumMode(Platform *aiPlatform, int targetY, const Borders *borders)
+{
+    int random = rand() % 10;
+    int currentY = aiPlatform->Y;
+    int needDown = currentY < targetY;
+    if (random <= 1) // в 20% случаев двигаемся не туда
+    {
+        MovePlatform(aiPlatform, borders, !needDown);
+    } 
+    else if (random >= 6) // в 40 % случаев двигаемся куда надо 
+    {
+        MovePlatform(aiPlatform, borders, needDown);
+    }
+    // 2 .. 5 не двигаемся в40 % случаев
+    return;
+}
+
+void aiHardMode(Platform *aiPlatform, int targetY, const Borders *borders)
+{
+    int random = rand() % 20;
+    int currentY = aiPlatform->Y;
+    int needDown = currentY < targetY;
+    if (random == 0) // в 5% случаев двигаемся не туда
+    {
+        MovePlatform(aiPlatform, borders, !needDown);
+    } 
+    else if (random >= 4) // в 80 % случаев двигаемся куда надо 
+    {
+        MovePlatform(aiPlatform, borders, needDown);
+    }
+    // 1 .. 3 не двигаемся в40 % случаев
+    return;
+}
---
+++ src/aiplayer.h
@@ -0,0 +1,35 @@
+#ifndef AIPLAYER_H
+#define AIPLAYER_H
+
+#include "ball.h"
+// #include "borders.h"  
+// #include "platfrom.h" 
+
+// TODO убрать заглушки после мерджа
+typedef struct {
+    int Y;
+    int heightPlatforms;  // высота платформы
+} Platform;
+
+typedef struct {
+    int height;  // высота поля
+} Borders;
+
+void MovePlatform(Platform *Platforms, Borders* field, int isDown) {}
+// вот до сюда
+
+typedef enum {
+    E_EASY = 1,
+    E_MEDIUM,
+    E_HARD,
+    E_GODMOD
+} AIPlayerLevel;
+
+/// @brief Метод который обрабатывает действия ИИ игрока, при самой низкой сложности будет "забывать" делать ход или делать в другую сторону
+/// @param aiPlatform указатель на платформу ИИ игрока
+/// @param ball Указатель на мяч
+/// @param aiLevel сложность игры
+void aiMovePlatform(Platform *aiPlatform, const Ball *ball, const Borders *borders, AIPlayerLevel aiLevel);
+
+
+#endif //AIPLAYER_H
